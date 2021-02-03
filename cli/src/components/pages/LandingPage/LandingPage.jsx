ddfcd7e774c9414ce50b56cc2d3386d893211983
mocha <syda07@gmail.com>
2021-02-03 19:25:31 +0900

    💄 랜딩페이지 뭔가 맘에 안들지만 일단 glassmorphism style 적용

--- cli/src/components/pages/LandingPage/LandingPage.jsx
+++ cli/src/components/pages/LandingPage/LandingPage.jsx
@@ -2,47 +2,44 @@ import React from 'react';
 
 import styled from 'styled-components';
 import Button from '../../Button/Button';
+
 import './LandingPage.css';
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-`;
-
-const Row = styled.div`
-  display: flex;
-  flex-direction: row;
-`;
+const Wrapper = styled.div``;
 
 const LandingPage = () => {
   return (
-    <Wrapper>
-      <h1>K-pop Master Quiz</h1>
-      <div style={{ marginTop: '100px' }}>
-        <div>
-          <h3>주의사항</h3>
+    <div className='wrapper'>
+      <div className='white'>
+        <div className='title'>
+          <h1>
+            K-pop
+            <br /> Master <br />
+            Quiz
+          </h1>
         </div>
-        <div>현재는 블랙핑크로 테스트 중입니다!</div>
-        <div style={{ marginTop: '40px' }}>
-          <ul>
-            <h4>개발 예정 리스트</h4>
-            <li>00년대 케이팝</li>
-            <li>10년대 케이팝</li>
-            <li>20년대 케이팝</li>
-            <li>9080 케이팝</li>
-            <li>가수 (검색 지원)</li>
-            <li>10곡 이상 선택 가능</li>
-            <li>힌트 유무 선택 가능</li>
-          </ul>
+        <div className='alert'>
+          <div className='alert-text'>
+            <div className='mark'>알림</div>
+            <p>현재는 블랙핑크로 테스트 중입니다!</p>
+          </div>
+          <div className='list'>
+            <div className='blue'>개발 예정 리스트</div>
+            <ul>
+              <li>00년대 가요</li>
+              <li>10년대 가요</li>
+              <li>20년대 가요</li>
+              <li>9080 가요</li>
+              <li>가수 (검색 지원)</li>
+              <li>힌트 유무 선택 가능</li>
+            </ul>
+          </div>
+          <div className='button-container'>
+            <Button child={'START'} links={'/start'} />
+          </div>
         </div>
       </div>
-      <div></div>
-      <Row>
-        <Button child={'PRESS TO START'} links={'/start'} />
-      </Row>
-    </Wrapper>
+    </div>
   );
 };
 export default LandingPage;
