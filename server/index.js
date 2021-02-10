cd1b63e961d897550c9e1257018c037e68087a29
mocha <syda07@gmail.com>
2021-02-11 04:02:55 +0900

    🔒 dotEnv 사용!

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const config = require('./config/key');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const mongo = process.env.MONGO_URI;
+
 mongoose
-  .connect(config.mongoURI, {
+  .connect(mongo, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -21,6 +22,7 @@ mongoose
 
 app.use('/api/chart', require('./routes/chart'));
 app.use('/api/user', require('./routes/user'));
+app.use('/api/youtube', require('./routes/youtube'));
 
 app.listen(5000, () => {
   console.log(`running on 5000`);
