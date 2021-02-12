58604f09ef997c401c2a60cb386ff0a791db087e
mocha <syda07@gmail.com>
2021-02-13 00:52:12 +0900

    🩹

--- server/model/User.js
+++ server/model/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema(
   {
-    userName: { type: String, unique: 1, maxLength: 10 },
+    userName: { type: String, unique: true, maxLength: 10 },
     record: Number,
     correctTrackName: Array,
     wrongTrackName: Array,
