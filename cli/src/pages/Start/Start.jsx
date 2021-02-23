aaac799c03b558a92b263eb39fc3aaf52f7d22a1
mocha <syda07@gmail.com>
2021-02-24 03:33:16 +0900

    ✨ 뒤로가기, refresh 방지 알람 뜸

--- cli/src/pages/Start/Start.jsx
+++ cli/src/pages/Start/Start.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Prompt } from 'react-router-dom';
 
 import GameLayout from '../GameLayout/GameLayout';
 import OutroPage from '../OutroPage/OutroPage';
@@ -14,11 +15,29 @@ const Start = () => {
     TrackListToPlayContext
   );
 
+  useEffect(() => {
+    window.addEventListener('beforeunload', alertUser);
+
+    return () => {
+      window.removeEventListener('beforeunload', alertUser);
+    };
+  }, []);
+
+  const alertUser = (e) => {
+    e.preventDefault();
+    e.returnValue = '';
+  };
+
   if (isGameEnd) {
     return <OutroPage />;
   }
-  console.log(trackListToPlay.trackList);
-  return <GameLayout trackList={trackListToPlay.trackList} />;
+
+  return (
+    <>
+      <Prompt message='플레이 도중에 나갈 경우, 플레이 기록을 잃습니다.' />
+      <GameLayout trackList={trackListToPlay.trackList} />
+    </>
+  );
 };
 
 export default Start;
