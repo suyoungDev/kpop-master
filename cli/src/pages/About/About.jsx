d7979bf04b434f0ac19616903dd74a307b98ec5e
mocha <syda07@gmail.com>
2021-02-24 03:35:28 +0900

    ✏️ about페이지,how to play 글귀 수정

--- cli/src/pages/About/About.jsx
+++ cli/src/pages/About/About.jsx
@@ -40,16 +40,13 @@ const About = () => {
       <GameTitle about>mocha</GameTitle>
       <Content bottom>
         <p>
-          안녕하세요! 재밋게 즐기셨나요?{' '}
+          안녕하세요! 재밋게 즐기셨나요?
           <span role='img' aria-labelledby='smile'>
             😙
           </span>
         </p>
         <p>문의사항이나 건의사항이 있다면 아래의 메일로 보내주세요.</p>
-        <Content>
-          <p>해당 프로젝트는 취업 용 포트폴리오입니다.</p>
-          <p>만약 제 포트폴리오가 마음에 드셨다면, 연락 바랍니다!</p>
-        </Content>
+
         <a
           href='https://github.com/mochapoke/kpop-master/'
           title='깃허브 페이지로 이동'
