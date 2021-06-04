f3a8c0f2b1660bf18775c4414ddd60e5425b6742
mocha <syda07@gmail.com>
2021-06-05 07:55:31 +0900

    ♻️ chart.js 리팩토링

--- server/routes/chart.js
+++ server/routes/chart.js
@@ -4,88 +4,79 @@ var melon = require('melon-chart-parser');
 
 const regex = /[(]/g;
 
+const filtering = (data) => {
+  const result = data
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 5)
+    .map((song) => ({
+      trackName: song.trackName.split(regex)[0],
+      artistName: song.artistName.split(regex)[0],
+    }));
+
+  return result;
+};
+
 router.post('/getByArtist', async (req, res) => {
-  var opts = {
+  const opts = {
     limit: req.body.limit,
     type: req.body.theme,
     term: req.body.value,
   };
 
-  var result = await melon
-    .parse(opts)
-    .then(function (res) {
-      return res.map((song) => ({
-        trackName: song.trackName,
-        artistName: song.artistName,
-      }));
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+  let result;
 
-  const shuffled = result.sort(() => Math.random() - 0.5).slice(0, 5);
-  const filtered = shuffled.map((song) => ({
-    trackName: song.trackName.split(regex)[0],
-    artistName: song.artistName.split(regex)[0],
-  }));
-  res.status(200).json({ success: true, result: filtered });
+  try {
+    let parsedData = await melon.parse(opts);
+    result = await filtering(parsedData);
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.status(200).json({ success: true, result });
 });
 
 router.post('/getByYear', async (req, res) => {
   let result = [];
-  console.log(req.body);
   let year = Number(req.body.value);
   let finallYear = year === 2020 ? 2020 : Number(req.body.value) + 9;
+
   for (let i = year; i <= finallYear; i++) {
-    var opts = {
+    const opts = {
       limit: req.body.limit,
       type: req.body.theme,
       genre: 'KPOP',
       year: i,
     };
-    const data = await melon
-      .parse(opts)
-      .then((res) => {
-        return res;
-      })
-      .catch((err) => console.log(err));
 
-    result.push(...data);
+    try {
+      const parsedData = await melon.parse(opts);
+      result.push(...parsedData);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   const shuffled = result.sort(() => Math.random() - 0.5).slice(0, 5);
-  const filtered = shuffled.map((song) => ({
-    trackName: song.trackName.split(regex)[0],
-    artistName: song.artistName,
-  }));
+  const filtered = filtering(shuffled);
 
   res.status(200).json({ success: true, result: filtered });
 });
 
 router.post('/getByWeek', async (req, res) => {
-  var opts = {
+  const opts = {
     limit: req.body.limit,
     type: req.body.theme,
   };
 
-  var result = await melon
-    .parse(opts)
-    .then(function (res) {
-      return res.map((song) => ({
-        trackName: song.trackName,
-        artistName: song.artistName,
-      }));
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+  let result;
+  try {
+    const parsedData = await melon.parse(opts);
+    result = filtering(parsedData);
+  } catch (error) {
+    console.log(error);
+  }
 
-  const shuffled = result.sort(() => Math.random() - 0.5).slice(0, 5);
-  const filtered = shuffled.map((song) => ({
-    trackName: song.trackName.split(regex)[0],
-    artistName: song.artistName,
-  }));
-
-  res.status(200).json({ success: true, result: filtered });
+  res.status(200).json({ success: true, result });
 });
+
 module.exports = router;
