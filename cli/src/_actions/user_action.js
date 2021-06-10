450445d392470cf98b611ddb560814b78114813d
mocha <syda07@gmail.com>
2021-06-11 04:03:07 +0900

    ♻️ async/await

--- cli/src/_actions/user_action.js
+++ cli/src/_actions/user_action.js
@@ -1,33 +1,29 @@
-import Axios from 'axios';
+import axios from 'axios';
 import ActionTypes from './types';
 
-export function loginUser(dataToSubmit) {
-  const request = Axios.post('/api/user/login', dataToSubmit).then(
-    (response) => response.data
-  );
+export const loginUser = async (dataToSubmit) => {
+  const { data } = await axios.post('/api/user/login', dataToSubmit);
 
   return {
     type: ActionTypes.LOGIN_USER,
-    payload: request,
+    payload: data,
   };
-}
+};
 
-export function registerUser(dataToSubmit) {
-  const request = Axios.post('/api/user/register', dataToSubmit).then(
-    (response) => response.data
-  );
+export const registerUser = async (dataToSubmit) => {
+  const { data } = await axios.post('/api/user/register', dataToSubmit);
 
   return {
     type: ActionTypes.REGISTER_USER,
-    payload: request,
+    payload: data,
   };
-}
+};
 
-export function auth() {
-  const request = Axios.get('/api/user/auth').then((response) => response.data);
+export const auth = async () => {
+  const { data } = await axios.get('/api/user/auth');
 
   return {
     type: ActionTypes.AUTH_USER,
-    payload: request,
+    payload: { data },
   };
-}
+};
