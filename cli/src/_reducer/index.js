52810a568ea4b50ebcd51c5be94b3ef2ec627609
mocha <syda07@gmail.com>
2021-06-11 04:35:48 +0900

    🎨 redux-devtools 적용해서 코드 깔끔하게 수정 / redux store 수정

--- cli/src/_reducer/index.js
+++ cli/src/_reducer/index.js
@@ -1,8 +1,16 @@
-import { combineReducers } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
 import user from './user_reducer';
 
 const rootReducer = combineReducers({
   user,
 });
 
-export default rootReducer;
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, promise))
+);
+
+export default store;
--- cli/src/index.js
+++ cli/src/index.js
@@ -1,29 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
-import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import ReduxThunk from 'redux-thunk';
-import promiseMiddleware from 'redux-promise';
 
-import Reducer from './_reducer';
-
+import store from './_reducer';
 import App from './App';
 
-const createStoreWithMiddleware = applyMiddleware(
-  promiseMiddleware,
-  ReduxThunk
-)(createStore);
-
 ReactDOM.render(
-  <Provider
-    store={createStoreWithMiddleware(
-      Reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )}
-  >
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
