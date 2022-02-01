1b0c7a69528c4f6f5ca9223b503bfd254a22efd8
suyoung <syda07@gmail.com>
2022-02-02 02:16:51 +0900

    fix: trackForm 스토리북 수정

--- frontend/src/components/foundation/TrackForm/index.stories.tsx
+++ frontend/src/components/foundation/TrackForm/index.stories.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import TrackForm, { Props } from './index';
-import useInput from '@HOOK/useInput';
+import useInputs from '@HOOK/useInputs';
+import { TrackInfo } from '@TS/track';
 
 export default {
   title: 'foundation/TrackForm',
   components: TrackForm,
 } as ComponentMeta<typeof TrackForm>;
 
+const FORM: TrackInfo = { trackName: '', artistName: '', videoId: '' };
+
 const Template: ComponentStory<typeof TrackForm> = (args: Props) => {
-  return <TrackForm />;
+  const { inputs, onChange } = useInputs<TrackInfo>([FORM], FORM);
+
+  return (
+    <div>
+      {inputs.map((input, idx) => (
+        <TrackForm values={input} setValues={onChange(idx)} key={idx} />
+      ))}
+    </div>
+  );
 };
 
 export const Default = Template.bind({});
