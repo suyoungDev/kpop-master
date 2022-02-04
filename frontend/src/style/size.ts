37217549271556a28cbe816327c0db7731fcbd94
suyoung <syda07@gmail.com>
2022-02-04 17:49:22 +0900

    remove: 필요없는 코드,파일 삭제

--- frontend/src/style/size.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  padding50: '1rem',
-  borderRadius50: '0.6rem',
-  borderRadius40: '0.3rem',
-};
--- frontend/src/types/styles.ts
+++ frontend/src/types/styles.ts
@@ -5,4 +5,3 @@ export type MediaQuery = 'mobile' | 'tablet' | 'laptop' | 'tablet' | 'desktop' |
 export type Colors = 'success' | 'danger' | 'info' | 'warning';
 export type Action = 'default' | 'hover' | 'act' | 'disabled';
 export type Dim = 'default' | 'light' | 'lighter' | 'dark' | 'darker';
-// export type NodeElement = 'background' | 'border' | 'text';
