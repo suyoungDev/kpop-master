ea10500973f0e03b119ab053307ba2f2c4fe56a1
suyoung <syda07@gmail.com>
2022-02-09 01:36:51 +0900

    feat: 저장할 url 아이템이 없을경우 videoId 를 검색하여 가져온뒤 새로 저장하는 기능 추가

--- server/routes/urls.js
+++ server/routes/urls.js
@@ -3,31 +3,38 @@ const router = express.Router();
 const { Url } = require('../model/Url');
 const { isExistTrack, getExistTrack } = require('../function/url');
 const { successResponse, failResponse } = require('../function/response');
+const getVideoId = require('../function/youtube');
 
-/** NOTE: req.body의 정보를 토대로 Urls콜렉션에 존재유무 확인후, Urls에 저장함
- * videoId는 FE에서 req.body에 전달해줘야함 -_-
+const saveNewTrack = (response, trackInfo) => (videoId) => {
+  const newVideoId = new Url({ ...trackInfo, videoId });
+  newVideoId.save((error, doc) => {
+    if (error) throw new Error(error);
+    return successResponse(response)(doc);
+  });
+};
+
+/**
+ * 1. req.body로 받은 정보의 트랙이 존재하는지 확인
+ * 2. 없을 경우, videoId를 받아서 새로운 url로 저장 후 정보 전달
  */
 router.post('/', async (req, res) => {
   try {
     const isExist = await isExistTrack(req.body);
-    if (isExist) {
-      return res.status(200).json({ success: true, message: 'already exist' });
-    } else {
-      const newVideoId = new Url(req.body);
-      newVideoId.save((error, doc) => {
-        if (error) throw new Error(error);
-        return successResponse(res)(doc);
-      });
-    }
+    if (isExist) return failResponse(res, 'already exist');
+    return await getVideoId(req.body, saveNewTrack(res, req.body));
   } catch (error) {
-    return failResponse(res);
+    return failResponse(res, error);
   }
 });
 
 router.get('/', async (req, res) => {
-  const doc = await getExistTrack(req.body);
-  if (doc.length) return successResponse(res)(doc);
-  else return failResponse(res);
+  try {
+    const doc = await getExistTrack(req.body);
+    if (doc.length) return successResponse(res)(doc);
+    else return failResponse(res);
+  } catch (error) {
+    return failResponse(res, error);
+  }
 });
 
 module.exports = router;
